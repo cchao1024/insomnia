afaa48ce72da3f0e7c0e3198d380be8780692f2b
Jcxcc <xcc880@gmail.com>
2019-02-23 13:56:45 +0800

    :heavy_plus_sign: SystemErrorMessage

---
+++ src/main/java/com/cchao/sleep/exception/SystemErrorMessage.java
@@ -0,0 +1,28 @@
+package com.cchao.sleep.exception;
+
+import lombok.Getter;
+
+/**
+ * 系统错误
+ *
+ * @author Jcxcc
+ * @date 2019-02-23
+ * @since 1.0
+ */
+@Getter
+public enum SystemErrorMessage {
+
+    /**
+     * 100000 用户相关
+     */
+    USER_NOT_EXISTED(100000, "用户不存在"),
+    ;
+
+    SystemErrorMessage(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    private Integer code;
+    private String message;
+}
