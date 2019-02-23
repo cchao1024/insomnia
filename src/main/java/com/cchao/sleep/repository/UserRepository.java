2d19079abcc568e9978beabf38e1c7fbb6458da7
Jcxcc <xcc880@gmail.com>
2019-02-23 13:38:56 +0800

    :fire: remove useless code

--- src/main/java/com/cchao/sleep/repository/UserRepository.java
+++ src/main/java/com/cchao/sleep/repository/UserRepository.java
@@ -2,17 +2,17 @@ package com.cchao.sleep.repository;
 
 import com.cchao.sleep.dao.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+/**
+ * The interface User repository.
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Find by email user.
+     *
+     * @param name the name
+     * @return the user
+     */
     User findByEmail(String name);
-
-    User findByNameAndAge(String name, Integer age);
-
-    @Query("from User u where u.name=:name")
-    User findUser(@Param("name") String name);
-
-
 }
