de77dda26d68635bb22d3b913f29270277a98a68
Jcxcc <xcc880@gmail.com>
2019-02-23 13:37:06 +0800

    :children_crossing: improve ExceptionHandlers

--- src/main/java/com/cchao/sleep/handler/ExceptionHandlers.java
+++ src/main/java/com/cchao/sleep/handler/ExceptionHandlers.java
@@ -4,12 +4,15 @@ import com.cchao.sleep.exception.CommonException;
 import com.cchao.sleep.exception.UnauthorizedException;
 import com.cchao.sleep.json.RespBean;
 import org.apache.shiro.ShiroException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
 
 /**
  * @author : cchao
@@ -29,17 +32,38 @@ public class ExceptionHandlers {
         return RespBean.of(ex.getCode(), ex.getMessage());
     }
 
-    // 捕捉shiro的异常
+    /**
+     * 捕捉shiro的异常
+     */
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = ShiroException.class)
     public RespBean handle401(ShiroException e) {
         return RespBean.of(401, e.getMessage());
     }
 
-    // 捕捉UnauthorizedException
+    /**
+     * 捕捉UnauthorizedException
+     */
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = UnauthorizedException.class)
     public RespBean handle401(UnauthorizedException e) {
         return RespBean.of(401, e.getMessage());
+    }
+
+
+    /**
+     * 参数校验
+     *
+     * @param ex the ex
+     */
+    @ResponseStatus(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public RespBean methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ex.printStackTrace();
+        String errorMsg = ex.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(","));
+
+        return RespBean.of(203, errorMsg);
     }
 }
